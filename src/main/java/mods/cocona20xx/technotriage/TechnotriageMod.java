7a3439c8206eec5a45eca3144f80780a08bb1c32
Cocona <pexium128@gmail.com>
2022-06-02 00:31:30 -0400

    pushing to new repo

--- src/main/java/mods/cocona20xx/technotriage/TechnotriageMod.java
+++ src/main/java/mods/cocona20xx/technotriage/TechnotriageMod.java
@@ -1,18 +1,17 @@
 package mods.cocona20xx.technotriage;
 
+import net.minecraft.util.Identifier;
 import org.quiltmc.loader.api.ModContainer;
 import org.quiltmc.qsl.base.api.entrypoint.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class TechnotriageMod implements ModInitializer {
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod name as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
+
 	public static final Logger LOGGER = LoggerFactory.getLogger("technotriage");
 
 	@Override
 	public void onInitialize(ModContainer mod) {
-		LOGGER.info("Hello Quilt world from {}!", mod.metadata().name());
+
 	}
 }
